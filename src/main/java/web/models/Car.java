5e9cbaf648200a4a8899d0090fbbc1185db43352
KhabibullaevAli <khabibullaevali@gmail.com>
2022-08-30 16:09:36 +0300

    upd 30_08_22 added Equals and HashCode

--- src/main/java/web/models/Car.java
+++ src/main/java/web/models/Car.java
@@ -37,4 +37,22 @@ public class Car {
     public void setMaxLoad(int maxLoad) {
         this.maxLoad = maxLoad;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+
+        if (maxSpeed != car.maxSpeed) return false;
+        if (maxLoad != car.maxLoad) return false;
+        return model.equals(car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = model.hashCode();
+        result = 31 * result + maxSpeed;
+        result = 31 * result + maxLoad;
+        return result;
+    }
 }
